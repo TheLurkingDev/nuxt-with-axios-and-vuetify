6a34495d213979f12669cea523652a55b6fab560
TheLurkingDev <thelurkingdev@gmail.com>
2018-09-11 10:12:42 -0400

    Added Card component that uses Vuetify card. Passed dynamic data into Card component.

--- my-itunes-search/pages/results/_id.vue
+++ my-itunes-search/pages/results/_id.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        {{ $route.params.id }}
-        {{ albumData }}
+        <h1> Results for {{ $route.params.id }}</h1>
+        <div v-if="albumExists">
+            <div v-for="(album, index) in albumData" :key="album.id">
+                <card
+                    :title="album.collectionCensoredName"
+                    :image="album.artworkUrl100"
+                    :artistName="album.artistName"
+                    :url="album.artistViewUrl"
+                    :color="picker(index)"
+                ></card>
+            </div>            
+        </div>
+        <div v-else>
+            <h3>No matching albums found.</h3>
+        </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Card from '~/components/Card.vue';
 
 export default {
     asyncData({ params }) {
@@ -16,6 +30,19 @@ export default {
         });
     },
 
-    middleware: 'search'
+    middleware: 'search',
+    computed: {
+        albumExists() {
+            return this.albumData.length > 0;
+        }
+    },
+    components: {
+        Card
+    },
+    methods: {
+        picker(index) {
+            return index % 2 == 0 ? 'red' : 'blue';
+        }
+    }
 }
 </script>
