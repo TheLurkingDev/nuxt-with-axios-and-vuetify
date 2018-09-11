4aba3e53ae9849e84bfb6c48654d1ca32e33e0f2
TheLurkingDev <thelurkingdev@gmail.com>
2018-09-10 22:30:25 -0400

    Added 'search' middleware to pick up text from input box then submit to apple rest api via axios get request. Implemented vuex store to hold album data returned in response.

--- my-itunes-search/pages/results/_id.vue
+++ my-itunes-search/pages/results/_id.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
         {{ $route.params.id }}
+        {{ $store.state.albums }}
     </div>
 </template>
+
+<script>
+export default {
+    middleware: 'search'
+}
+</script>
